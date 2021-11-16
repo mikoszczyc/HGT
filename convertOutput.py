60a5659f2645487896f3f74c4e5215f1e3ac0abd
mikoszczyc <mikoszczyc@gmail.com>
2021-11-16 16:19:37 +0100

    Code cleaning. Changed output file formatting to match.

--- convertOutput.py
+++ convertOutput.py
@@ -1,16 +1,11 @@
 # this script converts our output format into AZiele's
 
-cR = open('output_files/crossedResult.csv','r')
-myidentity = open('output_files/hgt.myidentity.txt', 'r')
+cR = open('output_files/crossedResult.csv', 'r')
 taxonomy = open('input_files/taxonomy.csv', 'r')
-
 converted = open('output_files/convCR.csv', 'w')
 
 
 for line in cR:
     line = line.split()
-	# level = line[-1].split(sep=':')[1]
-    # percentage = line[2]
     # lvl percentage org1 org2 gene1 gene2 genus1 genus2
     converted.write(f'{line[-1].split(sep=":")[1]}\t{line[2]}\t{line[0].split(sep="|")[0]}\t{line[1].split(sep="|")[0]}\t{line[0].split(sep="|")[1]}\t{line[1].split(sep="|")[1]}\n')
-    #\tgenus:{}\tgenus:{}
--- main.py
+++ main.py
@@ -8,30 +8,30 @@ import create_objects
 
 if __name__ == '__main__':
 
-    # identity_file = id_file + '.identity'
     taxonomy_file = 'input_files/taxonomy.csv'
     output_dir = 'output_files'
-    fh = open('input_files/alignment.identity')
+    alignment_file = open('input_files/alignment.identity')
     taxonomy = create_objects.createTaxObj(taxonomy_file)
     hitPercentage = 0
-    levels = ['family', 'order', 'cl', 'phylum', 'sk']
+    taxonomy_levels = ['family', 'order', 'cl', 'phylum', 'sk']
     Path(output_dir).mkdir(parents=True, exist_ok=True)  # creates directory if it didn't exist before
 
     # number of all sequences
-    seq_number = fh.readline()
+    seq_number = alignment_file.readline()
 
     # get all ids in one array
     ids = []
-    for line in fh:
+    for line in alignment_file:
         ids.append(line.split()[0])
-    fh.seek(0)  # go back to beginning of the file
-    fh.readline()  # read first line (number of sequences)
+    alignment_file.seek(0)  # go back to beginning of the file
+    alignment_file.readline()  # read first line (number of sequences)
     hit = open(f'{output_dir}/hit.txt', 'w')
 
-    for line in fh:
+    for line in alignment_file:
         temp = line.split()
-        species = temp[0].split('|')[0]
-        protein = temp[0].split('|')[1]
+        analysed_species = temp[0].split('|')[0]
+        analysed_protein = temp[0].split('|')[1]
+        analysed_genus = taxonomy[analysed_species].genus
         # array containing percentage identity to other sequences
         percentage_identity = temp[1:]
         identity = []
@@ -41,18 +41,22 @@ if __name__ == '__main__':
 
         identity.sort(key=itemgetter(1), reverse=True)  # sort by % DESC
         found = False
-        originOrganism = taxonomy[species]
+        originOrganism = taxonomy[analysed_species]
 
         for organism in identity:
+            other_species = organism[0].split(sep='|')[0]
+            other_protein = organism[0].split(sep='|')[1]
+            other_genus = taxonomy[other_species].genus
+            similarityPercentage = organism[1]
             if found:
                 if organism[1] == hitPercentage:
-                    tmpOrganism = taxonomy[organism[0].split('|')[0]]
-                    for i, level in enumerate(levels):
+                    tmpOrganism = taxonomy[other_species]
+                    for i, level in enumerate(taxonomy_levels):
                         if level == 'family':
                             continue
                         if getattr(tmpOrganism, level) == getattr(originOrganism, level):
-                            hitPercentage = organism[1]
-                            hit.write(f'{species}|{protein}\t{organism[0]}\t{organism[1]}\tl:{levels[i-1]}\n')
+                            hitPercentage = similarityPercentage
+                            hit.write(f'{i+2}\t{taxonomy_levels[i-1]}\t{hitPercentage}\t{analysed_species}\t{other_species}\t{analysed_protein}\t{other_protein}\tgenus:{analysed_genus}\tgenus:{other_genus}\n')
                             break
                     continue
                 else:
@@ -60,29 +64,41 @@ if __name__ == '__main__':
             elif not found:
                 hitPercentage = 0
                 if organism[1] != 100.0:
-                    tmpOrganism = taxonomy[organism[0].split('|')[0]]
+                    tmpOrganism = taxonomy[other_species]
                     if (tmpOrganism.genus != originOrganism.genus) and (tmpOrganism.family != originOrganism.family):
                         found = True
-                        for i, level in enumerate(levels):
+                        for i, level in enumerate(taxonomy_levels):
                             if level == 'family':
                                 continue
                             if getattr(tmpOrganism, level) == getattr(originOrganism, level):
-                                hitPercentage = organism[1]
-                                hit.write(f'{species}|{protein}\t{organism[0]}\t{organism[1]}\tl:{levels[i-1]}\n')
+                                hitPercentage = similarityPercentage
+                                hit.write(f'{i+2}\t{taxonomy_levels[i-1]}\t{hitPercentage}\t{analysed_species}\t{other_species}\t{analysed_protein}\t{other_protein}\tgenus:{analysed_genus}\tgenus:{other_genus}\n')
                                 break
     hit.close()
 
-    hit = open(f'{output_dir}/hit.txt', 'r')
-    result = open(f'{output_dir}/crossedResult.csv', 'w')
-    hitRead = hit.readlines()
+# zapis do par do pliku (crossed)
+    inputFile = open(f'{output_dir}/hit.txt', 'r')
+    outputFile = open(f'{output_dir}/crossedResult.csv', 'w')
+    inputContent = inputFile.readlines()
     foundItems = []
 
-    for i, line in enumerate(hitRead):
-        for line2 in hitRead[(i+1):]:
-            temp = sorted(line.split()[:2])
-            pair = temp[0] + temp[1]
-            temp2 = sorted(line2.split()[:2])
-            pair2 = temp2[0] + temp2[1]
-            if (pair == pair2) and (pair not in foundItems):
-                foundItems.append(pair)
-                result.write(f'{line}')
+    for i, line in enumerate(inputContent):
+        line_organism1 = line.split(sep='\t')[3]
+        line_organism1_protein = line.split(sep='\t')[5]
+
+        line_organism2 = line.split(sep='\t')[4]
+        line_organism2_protein = line.split(sep='\t')[6]
+        for line2 in inputContent[(i+1):]:
+            line2_organism1 = line2.split(sep='\t')[3]
+            line2_organism1_protein = line2.split(sep='\t')[5]
+
+            line2_organism2 = line2.split(sep='\t')[4]
+            line2_organism2_protein = line2.split(sep='\t')[6]
+
+            if line_organism1 == line2_organism2 and line_organism2 == line2_organism1:
+                if line_organism1_protein == line2_organism2_protein and line_organism2_protein == line2_organism1_protein:
+                    foundItems.append(line.split(sep='\t'))
+
+    sorted(foundItems, key=itemgetter(3))
+    for line in foundItems:
+        outputFile.write('\t'.join(line))
